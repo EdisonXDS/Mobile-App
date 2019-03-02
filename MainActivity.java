3d45de2a78d76a538fb0a3ff842fcef0cb6ba56c
EdisonXDS <47428212+EdisonXDS@users.noreply.github.com>
2019-03-02 18:56:58 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,5 @@
 
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//}
+
 package com.example.ece387app;
 
 
@@ -18,10 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-/**
 
- This app displays an order form to order coffee.
- */
 public class MainActivity extends AppCompatActivity {
     int quantity=0;
     @Override
@@ -30,17 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(com.example.ece387app.R.layout.activity_main);
     }
 
-    /**
-
-     This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
         display(quantity++);
     }
-    /**
 
-     This method displays the given quantity value on the screen.
-     */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(com.example.ece387app.R.id.quantity_text_view);
         quantityTextView.setText("" + number);
